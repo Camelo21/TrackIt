690b5e05a66b89a1833e887203f3cb1d997af2b3
Camelo21 <98890899+Camelo21@users.noreply.github.com>
2025-03-23 23:22:11 -0500

    Initial Commit

---
+++ TrackIt/TrackItApp.swift
@@ -0,0 +1,32 @@
+//
+//  TrackItApp.swift
+//  TrackIt
+//
+//  Created by Camilo Melo bernal on 23/03/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TrackItApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ TrackItTests/TrackItTests.swift
@@ -0,0 +1,16 @@
+//
+//  TrackItTests.swift
+//  TrackItTests
+//
+//  Created by Camilo Melo bernal on 23/03/25.
+//
+
+import Testing
+
+struct TrackItTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
